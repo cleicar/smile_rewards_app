89e96c0b6d5da4f58b86fce403a515470b8e18d7
Cleiviane Costa <costa.cleiviane@gmail.com>
2025-02-18 20:48:22 -0300

    Move Authentication to a Custom Hook
    Dashboard.tsx only focuses on UI rendering now and a new hook handles the authentication logic (Separation of Concerns). This also makes the code cleaner and improve reusability if we need authentication elsewhere

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
+
+import useSmileAuth from "../hooks/useSmileAuth";
 
 import Balance from '../components/Balance';
 import Rewards from '../components/Rewards';
@@ -10,39 +11,8 @@ const GET_CUSTOMER_POINTS = gql`
   }
 `;
 
-const GET_JWT_TOKEN = gql`
-  query GetCustomerJwt($customerId: ID!) {
-    customerJwt(customerId: $customerId)
-  }
-`;
-
 export const Dashboard = () => {
-  const shopifyCustomerId = process.env.REACT_APP_SHOPIFY_CUSTOMER_ID;
-  const smileKey = process.env.REACT_APP_SMILE_PUBLIC_KEY;
-  
-  const { loading: loadingToken, error: errorToken, data: tokenData } = useQuery(GET_JWT_TOKEN, {
-    variables: { customerId: shopifyCustomerId },
-  });
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
-  useEffect(() => {
-    if (!loadingToken && tokenData?.customerJwt && !isAuthenticated) {
-      if (!window.SmileUI) return; // Smile.js not loaded
-
-      if (!smileKey) return; // Smile public key not set
-      try {
-        window.SmileUI.init({
-          channel_key: smileKey,
-          customer_identity_jwt: tokenData.customerJwt,
-        });
-
-        setIsAuthenticated(true);
-      } catch (error) {
-        console.error("Error initializing Smile.js:", error);
-      }
-    }
-  }, [loadingToken, tokenData, isAuthenticated, smileKey]);
+  const { isAuthenticated, loadingToken, errorToken } = useSmileAuth();
 
   const { loading, error, data } = useQuery(GET_CUSTOMER_POINTS);
 
