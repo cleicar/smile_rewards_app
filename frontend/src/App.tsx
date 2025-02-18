7a6e653b1b6df53d963113ca7458ee9dfdf302b8
Cleiviane Costa <costa.cleiviane@gmail.com>
2025-02-18 11:42:13 -0300

    Get data from backend with GraphQL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,9 @@
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient";
 
 import BasePage from "./pages/BasePage";
 import Dashboard from './pages/Dashboard';
@@ -6,13 +11,17 @@ import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <>
-      <Router>
-        <BasePage pageName="Dashboard">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-          </Routes>
-        </BasePage>
-      </Router>
+      <ApolloProvider client={client}>
+        <React.StrictMode>
+          <Router>
+            <BasePage pageName="Dashboard">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+              </Routes>
+            </BasePage>
+          </Router>
+        </React.StrictMode>
+      </ApolloProvider>
     </>
   );
 }
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,22 @@
-import { Balance } from '../components/Balance';
+import { gql, useQuery } from "@apollo/client";
+
+import Balance from '../components/Balance';
+
+const GET_CUSTOMER_POINTS = gql`
+  query GetCustomerPoints {
+    customerPoints
+  }
+`;
 
 function Dashboard() {
-  const points = 200;
+  const { loading, error, data } = useQuery(GET_CUSTOMER_POINTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p className="text-red-500">Error fetching points</p>;
 
   return (
     <div className="grid grid-cols-12 gap-6 mt-8">
-      <Balance points={points} />
+      <Balance customerPoints={ data?.customerPoints } />
     </div>
   );
 }
