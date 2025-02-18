b2bfd2b768ac8ce511d979ee76cbd6b02c509e4f
Cleiviane Costa <costa.cleiviane@gmail.com>
2025-02-18 16:59:00 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import BasePage from "./pages/BasePage";
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
-      <Router>
-        <BasePage pageName="Dashboard">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-          </Routes>
-        </BasePage>
-      </Router>
+      <BasePage pageName="Dashboard">
+        <Dashboard />
+      </BasePage>
     </>
   );
 }
