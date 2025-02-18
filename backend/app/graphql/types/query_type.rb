8f26f035d6e235932bfc4e7e95cf393e162841b4
Cleiviane Costa <costa.cleiviane@gmail.com>
2025-02-18 20:38:27 -0300

    Load Smile.js and authenticate with JWT

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -2,8 +2,16 @@
 
 module Types
   class QueryType < Types::BaseObject
+    field :customer_jwt, String, null: false do
+      argument :customer_id, ID, required: true
+    end
+
     field :customer_points, Integer, null: false
 
+    def customer_jwt(customer_id:)
+      SmileJwtService.generate_jwt(customer_id)
+    end
+    
     def customer_points
       { points_balance: 200 }[:points_balance]
     end
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 
 import Balance from '../components/Balance';
@@ -9,11 +10,44 @@ const GET_CUSTOMER_POINTS = gql`
   }
 `;
 
-function Dashboard() {
+const GET_JWT_TOKEN = gql`
+  query GetCustomerJwt($customerId: ID!) {
+    customerJwt(customerId: $customerId)
+  }
+`;
+
+export const Dashboard = () => {
+  const shopifyCustomerId = process.env.REACT_APP_SHOPIFY_CUSTOMER_ID;
+  const smileKey = process.env.REACT_APP_SMILE_PUBLIC_KEY;
+  
+  const { loading: loadingToken, error: errorToken, data: tokenData } = useQuery(GET_JWT_TOKEN, {
+    variables: { customerId: shopifyCustomerId },
+  });
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  useEffect(() => {
+    if (!loadingToken && tokenData?.customerJwt && !isAuthenticated) {
+      if (!window.SmileUI) return; // Smile.js not loaded
+
+      if (!smileKey) return; // Smile public key not set
+      try {
+        window.SmileUI.init({
+          channel_key: smileKey,
+          customer_identity_jwt: tokenData.customerJwt,
+        });
+
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.error("Error initializing Smile.js:", error);
+      }
+    }
+  }, [loadingToken, tokenData, isAuthenticated, smileKey]);
+
   const { loading, error, data } = useQuery(GET_CUSTOMER_POINTS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p className="text-red-500">Error fetching points</p>;
+  if (loading || loadingToken || !isAuthenticated) return <p>Loading...</p>;
+  if (error || errorToken) return <p className="text-red-500">Error fetching data</p>;
 
   return (
     <div className="grid grid-cols-12 gap-6 mt-8">
